b9005c94f5e7d90559f52dbd644397545e944e70
patipat <lpatipat@icloud.com>
2022-06-17 22:48:49 -0400

    remove extra details + add univers 55 font

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
 	return (
@@ -10,18 +9,6 @@ export default function Home() {
 			<section className="hero">
 				<h1 className="hero__title">SAGE</h1>
 				<h2 className="hero__subtitle">Crafting most fair products in Web3</h2>
-			</section>
-			<section className="second">
-				<div className="second__circle">
-					<h1>A Circle Based Ecosystem</h1>
-				</div>
-			</section>
-			<section className="third">
-				<div>NFT</div>
-				<div>ASH</div>
-				<div>DEFI</div>
-				<div>TAU</div>
-				<div>DAO</div>
 			</section>
 		</div>
 	);
